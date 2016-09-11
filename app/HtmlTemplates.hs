da262db3afae56f1694fa07110a8d44d55eca293
theNerd247 <noah.harvey247 gm>
2016-09-11 00:59:27 -0400

    init test server
      * added home page route
      * set default route to serve directory to help locate css and js files

---
+++ app/HtmlTemplates.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-|
+Module      : HtmlTemplates
+Description : Short description
+Copyright   : (c) Some Guy, 2013
+License     : GPL-3
+Maintainer  : sample@email.com
+Stability   : experimental
+Portability : POSIX
+
+-}
+module HtmlTemplates
+  ( pageTemplate
+  ) where
+
+import Data.Text
+import Text.Blaze.Html ((!))
+import qualified Text.Blaze.Html5 as Html
+import qualified Text.Blaze.Html5.Attributes as HtmlA
+import qualified Text.Blaze.Bootstrap as BHtml
+
+bootstrapCSS :: Html.AttributeValue
+bootstrapCSS = "include/bootstrap-3.3.7-dist/css/bootstrap.min.css"
+
+bootstrapJS :: Html.AttributeValue
+bootstrapJS = "include/bootstrap-3.3.7-dist/js/bootstrap.min.js"
+
+bootStrapMeta :: Html.Html
+bootStrapMeta =
+  mconcat $
+  ((Html.meta !) . mconcat) <$>
+  [ [HtmlA.charset "utf-8"]
+  , [HtmlA.httpEquiv "X-UA-compatible", HtmlA.content "IE=edge"]
+  , [HtmlA.name "viewport", HtmlA.content "width=device-width, initial-scale=1"]
+  ]
+
+jqueryJS :: Html.AttributeValue
+jqueryJS = "include/jquery-3.1.0/js/jquery-3.1.0.min.js"
+
+importStyleSheets = mconcat . fmap BHtml.stylesheet
+
+importJS = mconcat . fmap BHtml.javascript
+
+pageTemplate :: Text -> Html.Html -> Html.Html
+pageTemplate ttle bdy =
+  Html.docTypeHtml $
+  do Html.head $
+       do bootStrapMeta
+          Html.title (Html.toHtml ttle)
+          importStyleSheets [bootstrapCSS]
+     Html.body $
+       do BHtml.container bdy
+          importJS [bootstrapJS, jqueryJS]
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Happstack.Lite
+import HtmlTemplates
+import qualified Text.Blaze.Html5 as Html
+import qualified Text.Blaze.Html5.Attributes as HtmlA
+import Text.Blaze.Html5 ((!))
+
+defaultResponse =
+  serveDirectory DisableBrowsing [] "/home/noah/src/com/ariaRacer"
+
+serverReponse = msum [dir "home" $ (ok $ toResponse testPage), defaultResponse]
+
+testPage = pageTemplate "Home" $ Html.div ! HtmlA.class_ "starter-template" $ helloWorld
+
+helloWorld = Html.h1 . Html.toHtml $ ("hello World" :: String)
 
 main :: IO ()
-main = someFunc
+main = serve Nothing serverReponse
