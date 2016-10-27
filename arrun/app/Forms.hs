8cb1b35e75b1d7d7ec0bdb804665ef902efc5b15
theNerd247 <noah.harvey247 gm>
2016-10-27 00:34:18 -0400

    finish form validation for racers

--- arrun/app/Forms.hs
+++ arrun/app/Forms.hs
@@ -10,21 +10,35 @@ import Data.Text (Text(..))
 import Happstack.Server
 import Text.Blaze.Html5 (Html)
 import Text.Blaze.Bootstrap
-import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html
 import Text.Reform hiding (view)
 import Text.Reform.Blaze.Common (setAttr)
 import Text.Reform.Blaze.Text
 import Text.Reform.Happstack
 import Web.Routes.PathInfo
+import qualified Text.Reform.Generalized as G
+import qualified Text.Blaze.Html5 as H
+import qualified Data.List as DL
+import qualified Text.Blaze.Html5.Attributes as A
 import qualified Data.Text as Strict
 
 type PasteForm m a = (Happstack m, Monad m, Alternative m, Functor m) =>
-                     Form m [Input] () Html () a
+                     Form m [Input] PasteFormError Html () a
 
-instance FormError () where
-  type ErrorInputType () = [Input]
-  commonFormError _ = ()
+data PasteFormError
+  = WrongFileType
+  | BuildNameRequired
+  | CFE (CommonFormError [Input])
+  deriving (Show)
+
+instance FormError PasteFormError where
+  type ErrorInputType PasteFormError = [Input]
+  commonFormError = CFE
+
+instance ToMarkup PasteFormError where
+  toMarkup WrongFileType = H.string "You need to upload a .cpp file"
+  toMarkup BuildNameRequired = H.string "Build name required"
+  toMarkup (CFE e) = H.string "Something went wrong"
 
 type NewRacerFormData = Strict.Text
 
@@ -39,13 +53,41 @@ uploadCodeForm act handle = reform (form act) "upload-code" handle Nothing genUp
 
 genNewRacerForm :: PasteForm m NewRacerFormData
 genNewRacerForm =
-  buttonSubmit "Submit" (glyphicon "plus" <> glyphicon "user")`setAttr` (A.type_ "submit" <> A.class_ "btn btn-success") 
-  *> inputText "" `setAttr` (A.placeholder "New Racer Name" <> A.class_ "form-control")
+  buttonSubmit "Submit" (glyphicon "plus" <> glyphicon "user") `setAttr`
+  (A.type_ "submit" <> A.class_ "btn btn-success") *>
+  inputText "" `setAttr`
+  (A.placeholder "New Racer Name" <> A.class_ "form-control")
 
 genUploadCodeForm :: PasteForm m UploadCodeFormData
-genUploadCodeForm =
-  fieldset $ 
-    buttonSubmit "Submit" (glyphicon "plus" <> glyphicon "duplicate") `setAttr` (A.type_ "submit" <> A.class_ "btn btn-success") 
-    *> pure UploadCodeFormData 
-    <*> (inputText "" `setAttr` (A.placeholder "Build Name" <> A.class_ "form-control"))
-    <*> (view _1 <$> inputFile) `setAttr` (A.class_ "form-control")
+genUploadCodeForm = fieldset $ bootstrapError ++> (submitButton *> uploadForm)
+  where
+    uploadForm = pure UploadCodeFormData <*> buildName <*> buildFile
+    buildFile =
+      (view _1 <$> inputFile) `transformEither` cppFileProof `setAttr`
+      (A.class_ "form-control")
+    submitButton =
+      buttonSubmit "Submit" (glyphicon "plus" <> glyphicon "duplicate") `setAttr`
+      (A.type_ "submit" <> A.class_ "btn btn-success")
+    buildName =
+      inputText "" `transformEither` buildNameProof `setAttr`
+      (A.placeholder "Build Name" <> A.class_ "form-control")
+
+bootstrapError
+  :: (Monad m)
+  => PasteForm m ()
+bootstrapError = G.childErrors showErrors
+  where
+    showErrors [] = mempty
+    showErrors errs = mconcat $ alertBox BootAlertDanger . toHtml <$> errs
+
+buildNameProof :: Strict.Text -> Either PasteFormError Strict.Text
+buildNameProof t =
+  case Strict.null . Strict.strip $ t of
+    True -> Left BuildNameRequired
+    _ -> Right t
+
+cppFileProof :: FilePath -> Either PasteFormError FilePath
+cppFileProof f =
+  case (DL.isSuffixOf ".cpp" f) of
+    True -> Right f
+    _ -> Left WrongFileType
