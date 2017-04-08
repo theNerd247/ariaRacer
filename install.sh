df59acfa967825e2ad9aca19a967cbc76eacc4cb
theNerd247 <noah.harvey247 gm>
2017-04-08 01:38:10 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-prefix="install"
+prefix="$(pwd)/install"
 install="$prefix/usr/share/ariaracer"
-installList="css fonts js scripts" 
+bin="$prefix/usr/bin"
+installList="css fonts js scripts aria.yaml" 
 
 mkdir -p $install
 
 cp -v -r -u $installList $install
+
+mkdir -p $bin
+ln -s $install/scripts/run_aria.sh $bin/run_aria
+ln -s $install/scripts/stop_aria.sh $bin/stop_aria
